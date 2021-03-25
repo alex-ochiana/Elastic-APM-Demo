0fce6e9ee000e7013e420c3733c86a98742cdaa0
Alexandru Ochiana <alexandru.ochiana@everymatrix.com>
2021-03-25 22:13:39 +0200

    add dummy web-server

---
+++ web-server/src/main/java/com/oaa/elasticapm/webserver/controller/DummyEndpointsController.java
@@ -0,0 +1,38 @@
+package com.oaa.elasticapm.webserver.controller;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+@RestController
+public class DummyEndpointsController {
+
+	private final static Logger logger = LoggerFactory.getLogger(DummyEndpointsController.class);
+
+    @GetMapping("/random")
+	public DeferredResult<ResponseEntity<String>> random() {
+		DeferredResult<ResponseEntity<String>> result = new DeferredResult<>();
+		ForkJoinPool.commonPool().submit(() -> {
+			long waitTime = randomDelay();
+			result.setResult(ResponseEntity.ok("Waited " + waitTime + " seconds"));
+		});
+		return result;
+	}
+
+	private long randomDelay() {
+		long waitTime = ThreadLocalRandom.current().nextLong(9) + 1;
+		try {
+			  Thread.sleep(waitTime * 1000);
+		} catch (InterruptedException e) {
+			logger.error("Thread interrupted", e);
+			Thread.currentThread().interrupt();
+		}
+		return waitTime;
+  	}
+}
---
+++ web-server/src/main/java/com/oaa/elasticapm/webserver/service/DummyRequestsService.java
@@ -0,0 +1,37 @@
+package com.oaa.elasticapm.webserver.service;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class DummyRequestsService {
+    
+    private static final int DELAY_IN_MILLIS = 1000;
+    private static final String URL = "http://localhost:8080/random";
+    private static final Logger logger = LoggerFactory.getLogger(DummyRequestsService.class);
+    private static final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+    @PostConstruct
+    public void init() {
+        RestTemplate restTemplate = new RestTemplate();
+        logger.info("Executing requests with " + DELAY_IN_MILLIS + " millis delay between.");
+        executorService.scheduleWithFixedDelay(() -> {
+            String response = restTemplate.getForEntity(URL, String.class).getBody();
+            logger.info(response);
+        }, 5 * DELAY_IN_MILLIS, DELAY_IN_MILLIS, TimeUnit.MILLISECONDS);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        executorService.shutdownNow();
+    }
+}
